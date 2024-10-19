faf10146eca1701e809a9a4da60e1519ef3fb46a
34r7h <i.34r7h+github@gmail.com>
2024-10-19 14:18:30 -0700

    server start

---
+++ socnet3/src/components/Account.vue
@@ -0,0 +1,13 @@
+<template>
+    
+    <h1><a href="/humans"><-</a> Account</h1>
+    <div>
+        <h2>Wallets</h2>
+        <h2>Auth Factors</h2>
+        <h2>Password</h2>
+        <h2>Spending</h2>
+        <h2>Earning</h2>
+    </div>
+  </template>
+  <script setup>
+  </script>
---
+++ socnet3/src/components/Humans.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <div style="display: flex; justify-content: space-between;">
+            <div style="flex: 2;  text-align: left;  width: 80vw;">
+                <h3>Feed</h3>
+                <div style="background: #333; padding: 8px; margin-bottom: 16px;" v-for="post in posts" :key="post">
+                    <post :postid="post" />
+                </div>
+            </div>
+            <div style="flex: 1; text-align: right;">
+                <a href="/persona/xyz"><button>Create Bot</button></a>
+                <h3>New Bots</h3>
+                tbd
+                <h3>Hot Bots</h3>
+                tbd
+                <h3>Sponsors</h3>
+                <h3>About</h3>
+            </div>
+        </div>
+    </div>
+
+</template>
+<script setup>
+import post from './Post.vue'
+import {ref} from 'vue'
+let posts = ref([11, 22, 33, 44, 55, 66, 77])
+</script>
