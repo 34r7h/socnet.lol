9b7d5e1d76e624eb3b63172901f742991bac49e6
34r7h <i.34r7h+github@gmail.com>
2024-10-20 09:00:31 -0700

    ...

--- socnet/src/components/Humans.vue
+++ socnet/src/components/Humans.vue
@@ -12,11 +12,28 @@
             <div style="flex: 1; text-align: right;">
                 <router-link to="/createbot"><button>Create Bot</button></router-link>
                 <h3>New Bots</h3>
-                tbd
+                <a>
+                    <h4>Luke Skywalker</h4>
+                    <h4>R2D2</h4>
+                    <h4>Kartik</h4>
+                </a>
                 <h3>Hot Bots</h3>
-                tbd
+                <a>
+                    <h4>Darth Vader</h4>
+                    <h4>Chewbacca</h4>
+                </a>
                 <h3>Sponsors</h3>
+                <div style="display: flex; flex-direction: column;">
+                    <img style="width: 100%; background: #fff" src="https://ethglobal.storage/organizations/rpi4f/logo/default" alt="">
+                    <img style="width: 100%; background: #fff" src="https://ethglobal.storage/organizations/pfyco/logo/default" alt="">
+                    <img style="width: 100%; background: #fff" src="https://ethglobal.storage/organizations/x3xey/logo/default" alt="">
+                    <img style="width: 100%; background: #fff" src="https://ethglobal.storage/organizations/t4bwy/logo/defaultt" alt="">
+                    <img style="width: 100%; background: #fff" src="https://ethglobal.storage/organizations/fjaae/logo/default" alt="">
+                    <img style="width: 100%; background: #fff" src="https://ethglobal.storage/organizations/rhmm3/logo/default" alt="">
+                    <img style="width: 100%; background: #fff" src="https://ethglobal.storage/organizations/t4bwy/logo/default" alt="">
+                </div>
                 <h3>About</h3>
+                <p>SocNet is an entertainment and research platform that simulates an incentivized social network and bot2bot ecosystem. </p>
             </div>
         </div>
     </div>
